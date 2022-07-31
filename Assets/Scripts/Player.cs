00d7d7e7756323edab001143dc129fa079b3f33d
Cristian <cri.ghiorzi@gmail.com>
2022-07-31 14:54:04 +0200

    Repulsive force on players hit

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -19,14 +19,21 @@ public class Player : MonoBehaviour
     private float moveSpeed = 10.0f;
     [SerializeField]
     private float rotationSpeed = 1.0f;
-    
+    [SerializeField]
+    private float repulsiveForce = 5.0f;
+
     [Header("References")]
     [SerializeField]
     private GameObject platform;
     [SerializeField]
     private GameObject duckModel;
 
+    [Header("Debug")]
+    [SerializeField]
+    private bool debug;
+
     private Rigidbody _rb;
+    private Vector3 _debugRepulsiveForce;
 
     private void Awake() {
         _rb = GetComponent<Rigidbody>();
@@ -57,9 +64,36 @@ public class Player : MonoBehaviour
         }
     }
 
-    private void OnCollisionStay(Collision other) {
-        if (other.gameObject.tag == "Platform") {
-            transform.up = other.transform.up;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            Vector3 hitPoint = collision.contacts[0].point;
+            Vector3 direction = (transform.position - hitPoint).normalized;
+
+            if(debug)
+                Debug.Log("Repulsive Force direction: " + direction+" for "+this.gameObject.name);
+
+            direction = Vector3.ProjectOnPlane(direction, platform.transform.up).normalized;
+            Vector3 force = direction * repulsiveForce;
+
+            _debugRepulsiveForce = force;
+            _rb.AddForce(force,ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionStay(Collision collision) {
+        if (collision.gameObject.tag.Equals("Platform")) {
+            transform.up = collision.transform.up;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (debug)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, _debugRepulsiveForce);
         }
     }
 }
